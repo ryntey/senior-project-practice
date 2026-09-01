4edc457e2a6c1b13c34389cf9df48966f746d810
ryntey <alloteylauryn@gmail.com>
2026-09-01 14:06:44 -0400

    Update app.py
    Updating app.py with a more realistic and personable answer.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 name = "Hi, my name is Lauryn Allotey"
 major = "my major is Computer Science" 
 tech_area = "I'm interested in the environmental impacts of AI" 
-skill = "My speed and accuracy when it comes to building out applications from scratch."
+skill = "My speed and accuracy when it comes to building out applications from scratch. I'm excited for this senior project course!"
 
 print(name, ",", major, "and", tech_area, ". I hope to improve:", skill)
