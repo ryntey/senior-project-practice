929b1b22acd63f5959e7744b98a16e6dcc4e104d
ryntey <alloteylauryn@gmail.com>
2026-09-01 14:03:41 -0400

    Update app.py
    Update app.py with code of my introduction

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
+name = "Hi, my name is Lauryn Allotey"
+major = "my major is Computer Science" 
+tech_area = "I'm interested in the environmental impacts of AI" 
+skill = "My speed and accuracy when it comes to building out applications from scratch."
 
+print(name, ",", major, "and", tech_area, ". I hope to improve:", skill)
