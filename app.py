b0373f45c7e938bd985659ef2021b68e662ca9c2
ryntey <alloteylauryn@gmail.com>
2026-09-01 15:36:59 -0400

    Delete app.py
    Accidentally made it in the main branch

--- app.py
+++
@@ -1,6 +0,0 @@
-name = "Hi, my name is Lauryn Allotey"
-major = "my major is Computer Science" 
-tech_area = "I'm interested in the environmental impacts of AI" 
-skill = "My speed and accuracy when it comes to building out applications from scratch. I'm excited for this senior project course!"
-
-print(f"{name}, {major}, and {tech_area}. I hope to improve: {skill}")
