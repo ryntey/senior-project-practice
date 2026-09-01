c3bbbd56a3f9dfb334f7ee99e31bbb74acd80063
ryntey <alloteylauryn@gmail.com>
2026-09-01 15:44:03 -0400

    Update app.py
    Updating print statement to prioritize efficiency

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ name = "Hi, my name is Lauryn Allotey."
 major = "My major is Computer Science" 
 tech_area = "I'm interested in the environmental impacts of AI." 
 skill = "my speed and accuracy when it comes to building out applications from scratch. I'm excited for this senior project course!"
+print("{}, {}, and {}. My skill goal is to improve {}.".format(name, major, tech_area, skill))
 
-print(name, major, ", and", tech_area, "My skill goal is to improve")
+
+
