b05d86c47ae8aef3da8ed8bcf4e2af290897adcc
ryntey <alloteylauryn@gmail.com>
2026-09-01 17:38:37 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+name = "Lauryn Allotey"
+major = "Computer Science."
+tech_area = "environmental impacts of AI and the possible alternatives "
+skill_goal = "increase speed, efficiency, and accuarcy when building a project from scratch."
+print("Hi! My name is", name, ". My major is", major, "I am interested in the", tech_area, "One of my main skill_goals is to", skill_goal)
