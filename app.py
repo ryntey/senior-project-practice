107be495d5e63b9f6704984750556e03d34e7901
ryntey <alloteylauryn@gmail.com>
2026-09-01 14:13:38 -0400

    Update app.py
    Reformatting print statement for efficiency's sake

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ major = "my major is Computer Science"
 tech_area = "I'm interested in the environmental impacts of AI" 
 skill = "My speed and accuracy when it comes to building out applications from scratch. I'm excited for this senior project course!"
 
-print(name, ",", major, "and", tech_area, ". I hope to improve:", skill)
+print(f"{name}, {major}, and {tech_area}. I hope to improve: {skill}")
