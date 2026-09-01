daac6932ce4bb87ade02b0a863a977d198967351
ryntey <alloteylauryn@gmail.com>
2026-09-01 17:42:47 -0400

    Revert "Feature/student info"

--- app.py
+++
@@ -1,5 +0,0 @@
-name = "Lauryn Allotey"
-major = "Computer Science."
-tech_area = "environmental impacts of AI and the possible alternatives "
-skill_goal = "increase speed, efficiency, and accuarcy when building a project from scratch."
-print(f"Hi! My name is {name}. My major is {major}. I am interested in the {tech_area}. One of my main skill goals is to {skill_goal}.")
